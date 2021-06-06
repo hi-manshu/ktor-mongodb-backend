97395ccae6d74054c8a9a1d837d8a05b20d637e0
Himanshu Singh <himanshusingh@Himanshus-MacBook-Pro.local>
2021-06-06 23:14:57 +0530

    Added: Like/Dislike tesT

--- src/test/kotlin/com/himanshoe/feature/posts/domain/AddLikeDislikeUseCaseTest.kt
+++ src/test/kotlin/com/himanshoe/feature/posts/domain/AddLikeDislikeUseCaseTest.kt
@@ -2,13 +2,22 @@ package com.himanshoe.feature.posts.domain
 
 import com.himanshoe.MainCoroutineRule
 import com.himanshoe.feature.posts.repository.PostsRepository
+import com.himanshoe.runBlockingTest
+import com.himanshoe.util.SuccessResponse
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.ktor.http.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @ExperimentalCoroutinesApi
-internal class AddLikeDislikeUseCaseTest {
+class AddLikeDislikeUseCaseTest {
 
     @get:Rule
     var coroutineRule = MainCoroutineRule()
@@ -20,5 +29,45 @@ internal class AddLikeDislikeUseCaseTest {
     @Before
     fun setup() {
         addLikeDislikeUseCase = AddLikeDislikeUseCase(mockPostsRepository)
+    }
+
+    @Test
+    fun `should add like successfully`() {
+        return coroutineRule.runBlockingTest {
+            `given a post should add dislike successfully`()
+            // given
+            val input = Triple("12345", "67890", true)
+            // when
+            val result = addLikeDislikeUseCase.invoke(input) as SuccessResponse<Any>
+            // then
+            verify(mockPostsRepository).likeDislikePost(input.first, input.second, input.third)
+            MatcherAssert.assertThat(result.statusCode, CoreMatchers.equalTo(HttpStatusCode.OK))
+        }
+    }
+
+    @Test
+    fun `should add dislike successfully`() {
+        return coroutineRule.runBlockingTest {
+            `given a post should add like successfully`()
+            // given
+            val input = Triple("12345", "67890", true)
+            // when
+            val result = addLikeDislikeUseCase.invoke(input) as SuccessResponse<Any>
+            // then
+            verify(mockPostsRepository).likeDislikePost(input.first, input.second, input.third)
+            MatcherAssert.assertThat(result.statusCode, CoreMatchers.equalTo(HttpStatusCode.OK))
+        }
+    }
+
+    private suspend fun `given a post should add dislike successfully`() {
+        val successResponse = SuccessResponse<Any>(HttpStatusCode.OK, true)
+        whenever(mockPostsRepository.likeDislikePost(any(), any(), any()))
+            .thenReturn(successResponse)
+    }
+
+    private suspend fun `given a post should add like successfully`() {
+        val successResponse = SuccessResponse<Any>(HttpStatusCode.OK, true)
+        whenever(mockPostsRepository.likeDislikePost(any(), any(), any()))
+            .thenReturn(successResponse)
     }
 }
